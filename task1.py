978d6c1fb55b6a70521d09a9af6466d7174d8c7b
Beata Rudzka <beatarudzka89@gmail.com>
2020-01-22 09:58:39 +0100

    task1

---
+++ task1.py
@@ -0,0 +1,10 @@
+slownik = {"kawa": "czarna", "herbata": "zielona",
+           "cukier kostka": 4, 0: "zero"}
+
+print slownik[0]
+
+print slownik.keys()
+print list(slownik.keys())
+print slownik.values()
+
+print "herbata" in slownik
