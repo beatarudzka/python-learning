f2d0d6d9c964ffef03f439771d47610f73256053
Beata Rudzka <beatarudzka89@gmail.com>
2020-01-22 12:46:53 +0100

    task7

---
+++ task7.py
@@ -0,0 +1,19 @@
+# write a new list with even numbers
+
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+evenNumbers = []
+
+for number in a:
+    if number % 2 == 0:
+        evenNumbers.append(number)
+
+print(evenNumbers)
+
+
+# list comprehension
+
+newListWithEvenNumbers = [number % 2 for number in a]
+
+print(newListWithEvenNumbers)
